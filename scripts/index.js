3f6d9f0669e54a7588b1e54b5e053a89c18f21a7
Angel Peguero Jr <angel.peguero14@outlook.com>
2025-11-25 21:47:14 -0500

    Edit profile modal implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
