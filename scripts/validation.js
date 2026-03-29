dd202009c38d5b84173439cbb9b33f82d56e2735
Angel Peguero Jr <angel.peguero14@outlook.com>
2026-03-29 01:15:13 -0400

    Form validation messages created and tested

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,4 +1,29 @@
-const setEventListeners = (formEl) => {};
+const showInputError = (formEl, inputEl, errorMessage) => {
+  const errorMessageID = inputEl.id + "-error";
+  const errorMessageEl = document.querySelector("#" + errorMessageID);
+  errorMessageEl.textContent = errorMessage;
+};
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+
+  // TODO - checkInputValidity, toggleButtonState
+  // togleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      //toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
 
 const enableValidation = () => {
   const formList = document.querySelectorAll(".modal__form");
